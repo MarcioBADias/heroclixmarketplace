5b5d1a86d48d8240025d658b4cd4da5952c72804
marciobadias <marcio.b.a.dias@gmail.com>
2024-04-02 20:34:14 -0300

    Começando o form do menu

--- src/app.jsx
+++ src/app.jsx
@@ -27,6 +27,25 @@ const App = () => {
         <h1>Marketplace</h1>
       </header>
 
+      <div className="menu">
+        <form action="#">
+          <input type="text" />
+          <button></button>
+          <span>Procure pelo nome do seu herói</span>
+        </form>
+        <form action="">
+          <select name="" id="">
+            <option value=""></option>
+            <option value="">Wheels of Vengeance</option>
+          </select>
+          <span>Selecione uma coleção para procura</span>
+        </form>
+
+        <div className="login">
+          <p>Cadastre-se ou efetuar Login</p>
+        </div>
+      </div>
+
       <div className="cards">
         {cards.map((card) => (
           <div key={card.id} className="card">
