0da06848fa72500bd93c2331ed6d1a80810abb74
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-03-24 20:58:09 -0300

    Tirando filtro que ainda nao esta ok

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -2,31 +2,31 @@ import { useEffect, useState } from 'react'
 import { Card } from '../card'
 import * as S from './style'
 
-const base_praces = import.meta.env.VITE_API_COOLSTUFF_PRICES
+const base_prices = import.meta.env.VITE_API_COOLSTUFF_PRICES
 const pathImage = import.meta.env.VITE_PATH_HCREALMS_IMG
 
 const Cards = ({ searchTerm, selectedCollection }) => {
   const [cards, setCards] = useState([])
 
   useEffect(() => {
-    fetch(
-      base_praces,
-    )
+    fetch(base_prices)
       .then((r) => r.json())
       .then((data) => setCards(data))
       .catch(console.log)
   }, [])
 
-  // Filtra os cards com base no nome e na coleção
   const filteredCards = cards.filter((card) => {
-    const matchesSearch = card.CharacterName.toLowerCase()== searchTerm.toLowerCase()
-    const matchesCollection = selectedCollection ? card.collection === selectedCollection : true
+    const matchesSearch =
+      card.CharacterName.toLowerCase() == searchTerm.toLowerCase()
+    const matchesCollection = selectedCollection
+      ? card.collection === selectedCollection
+      : true
     return matchesSearch && matchesCollection
   })
 
   return (
     <S.Cards>
-      {filteredCards.map((card) => (
+      {cards.map((card) => (
         <Card
           key={card.collection}
           name={card.CharacterName}
